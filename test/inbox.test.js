db0b522ac80d2957f983d14aa512b77125b1fde0
angel.martin.local <angel.martin@babel.es>
2022-02-12 21:07:20 +0100

    Test mensaje por defecto y actualización del mensaje (nueva transacción)

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 const web3 = new Web3(ganace.provider());
 const { interface, bytecode } = require('../compile'); 
 
+const defaultContractMessage = 'Hi there!';
 let accounts;
 let inbox;
 
@@ -12,7 +13,7 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     // Use one of the accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] }) // create the obtejct to deploy
+        .deploy({ data: bytecode, arguments: [defaultContractMessage] }) // create the obtejct to deploy
         .send({ from: accounts[0], gas: '1000000' });   // send the transaction to specific account
 });
 
@@ -23,5 +24,17 @@ describe('Inbox', () => {
 
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, defaultContractMessage);
+    });
+
+    it('can change the message', async () => {
+        const newMessage = 'Bye there!';
+        await inbox.methods.setMessage(newMessage).send({ from: accounts[0], gas: '1000000' });
+        const currentMessage = await inbox.methods.message().call();
+        assert.equal(newMessage, currentMessage);
     });
 })
