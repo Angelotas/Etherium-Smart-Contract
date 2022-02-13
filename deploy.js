ddb772b964c99b7b8616c954e9a6eadaef00e23b
angel.martin.local <angel.martin@babelgroup.com>
2022-02-13 20:42:55 +0100

    Migración a versión 0.8.9 de solidity

--- deploy.js
+++ deploy.js
@@ -1,21 +1,22 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require('./compile');
 
 const fs = require('fs');
 const MNEMONIC = fs.existsSync(".secret") && fs.readFileSync(".secret").toString().trim();
 
-const provier = new HDWalletProvider(MNEMONIC, "https://rinkeby.infura.io/v3/78a18edf98c34dcfa7202bebff714a56");
-const web3 = new Web3(provier);
+const provider = new HDWalletProvider(MNEMONIC, "https://rinkeby.infura.io/v3/78a18edf98c34dcfa7202bebff714a56");
+const web3 = new Web3(provider);
 
 const deploy = async () => {
     // get the accounts from the provider
     const accounts = await web3.eth.getAccounts();
     // use one of the accounts to deploy the contract
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] }) // create the obtejct to deploy
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] }) // create the obtejct to deploy
         .send({ from: accounts[0], gas: '1000000' });   // send the transaction to specific account
     // return the address of the contract
+    provider.engine.stop();
     return result.options.address;
 };
 
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganace = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganace.provider());
-const { interface, bytecode } = require('../compile'); 
+const { abi, evm } = require('../compile');
 
 const defaultContractMessage = 'Hi there!';
 let accounts;
@@ -12,8 +12,8 @@ beforeEach(async () => {
     // Get list of all accounts
     accounts = await web3.eth.getAccounts();
     // Use one of the accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [defaultContractMessage] }) // create the obtejct to deploy
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [defaultContractMessage] }) // create the obtejct to deploy
         .send({ from: accounts[0], gas: '1000000' });   // send the transaction to specific account
 });
 
